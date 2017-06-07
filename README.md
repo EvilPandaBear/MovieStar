5c040e4b4663b3f91c8cd7f0a3d3eab7c6850497
thePhilX <Philipp@grossX.de>
2017-06-08 01:40:54 +0200

    passport kagge
    added
    package.json
    server.js (alte app.js)

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MovieStar
+
+
+// mongoose user-model
+var UserSchema = new Schema({
+    name: { type: String, required: true, unique: true, minlength: 5 },
+    password: { type: String, required: true, minlength: 8 },
+    email: { type: String, required: true, index: { unique: true } },
+    date_of_birth: { type: Date, required: true },
+    is_admin: { type: Boolean, required: true, default: false }
+});
--- api/models/User.js
+++ api/models/User.js
@@ -1,15 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    // TODO Passwort verschlüsseln bcrypt = require('bcrypt'),
+// Thanks to http://blog.matoski.com/articles/jwt-express-node-mongoose/
 
+// set up a mongoose model
 var UserSchema = new Schema({
-    username: { type: String, required: true, minlength: 5, index: { unique: true } },
-    password: { type: String, required: true, minlength: 8 },
-    email: { type: String, required: true, index: { unique: true } },
-    date_of_birth: { type: Date, required: true },
-    name: { type String, required: true, minlength: 3},
-    is_admin: { type: Boolean, required: true, default: false }
+  name: { type: String, unique: true, required: true },
+  password: { type: String, required: true }
 });
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+UserSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', UserSchema);
