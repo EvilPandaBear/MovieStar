5a828425ac6120333ff3694f772cefc202f8af29
EvilPandaBear <tom.halecker@gmx.net>
2017-06-11 19:52:36 +0200

    Add files via upload

--- app/movie-list/movie-list.component.js
+++ app/movie-list/movie-list.component.js
@@ -1,16 +1,19 @@
 'use strict';
 
-// Register `movieList` component, along with its associated controller and template
+// Register `phoneList` component, along with its associated controller and template
 angular.
   module('movieList').
   component('movieList', {
     templateUrl: 'movie-list/movie-list.template.html',
-    controller: function MovieListController($http) {
-          var self = this;
-          self.orderProp = 'age';
 
-          $http.get('movies/movies.json').then(function(response) {
-            self.movies = response.data;
-          });
+    controller: ['$http',
+        function MovieListController($http) {
+            var self = this;
+            self.orderProp = 'age';
+
+            $http.get('movies/movies.json').then(function(response) {
+                self.movies = response.data;
+            });
         }
-      });
+    ]
+  });
