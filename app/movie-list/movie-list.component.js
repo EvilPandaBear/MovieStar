d6aa0719222f3ce19eae8253a67eb44544e3ec00
thePhilX <Philipp@grossX.de>
2017-06-11 19:42:41 +0200

    mit "server"

--- app/movie-list/movie-list.component.js
+++ app/movie-list/movie-list.component.js
@@ -5,23 +5,12 @@ angular.
   module('movieList').
   component('movieList', {
     templateUrl: 'movie-list/movie-list.template.html',
-    controller: function MovieListController() {
-      this.movies = [
-        {
-            name: 'aaa',
-            snippet: 'mega cooler film 1',
-            age: 1
-        }, {
-            name: 'ccc',
-            snippet: 'super cooler film 2',
-            age: 2
-        }, {
-            name: 'bbb',
-            snippet: 'super cooler film 3',
-            age: 3
-        }
-      ];
+    controller: function MovieListController($http) {
+          var self = this;
+          self.orderProp = 'age';
 
-      this.orderProp = 'age';
-    }
-  });
+          $http.get('movies/movies.json').then(function(response) {
+            self.movies = response.data;
+          });
+        }
+      });
